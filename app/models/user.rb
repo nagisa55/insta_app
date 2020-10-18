8fbf6520c6faea8ae6ffece742b6896cad639cf6
github nagisa55 <kamomeeee00@yahoo.co.jp>
2020-10-18 22:41:33 +0900

    for heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@
 	validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 	has_many :microposts, dependent: :destroy
 	has_many :comments, dependent: :delete_all
-	has_many :relationships
+	has_many :relationships, dependent: :delete_all
 	has_many :followings, through: :relationships, source: :follow
 	has_many :reverse_of_relationship, class_name: "Relationship", foreign_key: "follow_id"
 	has_many :followers, through: :reverse_of_relationship, source: :user
